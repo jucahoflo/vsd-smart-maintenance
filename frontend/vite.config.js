70ff325adeec9d10717e7c541e1986ed9b559537
jucahoflo <ssidelect@gmail.com>
2026-07-31 14:16:49 -0500

    fix: forzar uso de empaquetador estandar para evitar bug de rolldown

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [] // No externalizamos nada
+      // Esto obliga a Vite a usar el empaquetador clásico y evita el bug de Rolldown
+      output: {
+        manualChunks: undefined
+      }
     }
   }
 })
